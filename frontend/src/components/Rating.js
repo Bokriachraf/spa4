7f179cb6610fbef1c8e2198799c0cf82e55d6144
achrafbokri <achrafbokri@gmail.com>
2023-02-24 22:30:54 +0100

    v12

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,71 +1,78 @@
-import React from "react";
-// import { Stack,Rating} from "@mui/material"
+import React, {useState} from "react";
+import { Stack,Rating} from "@mui/material"
 
 
 export default function Rate(props) {
   const { rating, numReviews } = props;
+  // const [value,setValue]= useState<number | null>(null)
+  // const handleChange = (
+  // _event: React.ChangeEvent<{}>,
+  // newValue: number | null
+  // ) => {
+  //   setValue(newValue)
+  // }
   return (
-    // <Stack spacing={2}>
-    //   <Rating/>
-    // </Stack>
+    <Stack spacing={3} >
+      <Rating value={4} />
+    </Stack>
     
-    <div className="rating">
-      <span>
-        <i
-          className={
-            rating >= 1
-              ? "fa fa-star"
-              : rating >= 0.5
-              ? "fa fa-star-half-o"
-              : "fa fa-star-o"
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 2
-              ? "fa fa-star"
-              : rating >= 1.5
-              ? "fa fa-star-half-o"
-              : "fa fa-star-o"
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 3
-              ? "fa fa-star"
-              : rating >= 2.5
-              ? "fa fa-star-half-o"
-              : "fa fa-star-o"
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 4
-              ? "fa fa-star"
-              : rating >= 3.5
-              ? "fa fa-star-half-o"
-              : "fa fa-star-o"
-          }
-        ></i>
-      </span>
-      <span>
-        <i
-          className={
-            rating >= 5
-              ? "fa fa-star"
-              : rating >= 4.5
-              ? "fa fa-star-half-o"
-              : "fa fa-star-o"
-          }
-        ></i>
-      </span>
-      <span>{numReviews + " reviews"}</span>
-    </div>
+    // <div className="rating">
+    //   <span>
+    //     <i
+    //       className={
+    //         rating >= 1
+    //           ? "fa fa-star"
+    //           : rating >= 0.5
+    //           ? "fa fa-star-half-o"
+    //           : "fa fa-star-o"
+    //       }
+    //     ></i>
+    //   </span>
+    //   <span>
+    //     <i
+    //       className={
+    //         rating >= 2
+    //           ? "fa fa-star"
+    //           : rating >= 1.5
+    //           ? "fa fa-star-half-o"
+    //           : "fa fa-star-o"
+    //       }
+    //     ></i>
+    //   </span>
+    //   <span>
+    //     <i
+    //       className={
+    //         rating >= 3
+    //           ? "fa fa-star"
+    //           : rating >= 2.5
+    //           ? "fa fa-star-half-o"
+    //           : "fa fa-star-o"
+    //       }
+    //     ></i>
+    //   </span>
+    //   <span>
+    //     <i
+    //       className={
+    //         rating >= 4
+    //           ? "fa fa-star"
+    //           : rating >= 3.5
+    //           ? "fa fa-star-half-o"
+    //           : "fa fa-star-o"
+    //       }
+    //     ></i>
+    //   </span>
+    //   <span>
+    //     <i
+    //       className={
+    //         rating >= 5
+    //           ? "fa fa-star"
+    //           : rating >= 4.5
+    //           ? "fa fa-star-half-o"
+    //           : "fa fa-star-o"
+    //       }
+    //     ></i>
+    //   </span>
+    //   <span>{numReviews + " reviews"}</span>
+    // </div>
   );
 }
